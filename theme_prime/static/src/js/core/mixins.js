84d987eec6a21c0b781a61469b69fbddac054a71
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2022-08-30 12:41:34 +0100

    Add addon

--- theme_prime/static/src/js/core/mixins.js
+++ theme_prime/static/src/js/core/mixins.js
@@ -304,7 +304,7 @@ var CartManagerMixin = {
             console.log("selection les parent")
             console.log(this.$el.parentsUntil());
             console.log(this);
-            console.log($(ev.currentTarget))
+            console.log($(params.currentTarget))
 }
         });
     },
