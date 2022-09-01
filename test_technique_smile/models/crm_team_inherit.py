e5efefad1a44f172e9a0007a301d23affead69de
khallout asmaa <asmaekhalout2015@gmail.com>
2022-09-02 00:03:02 +0100

    test

--- test_technique_smile/models/crm_team_inherit.py
+++ test_technique_smile/models/crm_team_inherit.py
@@ -3,9 +3,10 @@ from odoo import models, fields,_ , api
 class CrmTeamInherit(models.Model):
     _inherit = "crm.team"
 
-    members_contact = fields.One2many("res.partner",'team_id',string="Members (contact)")
-    Responsables = fields.Many2many('res.partner', 'team_contact_rel', 'partner_id', 'team_id')
 
-    @api.onchange('Responsables')
-    def Responsables_onchange(self):
-        return {'domain': {'Responsables': [('id', 'in', self.members_contact.ids)]}}
+    def _get_domain(self):
+        return [('id', 'in', self.members_contact.ids)]
+
+    members_contact = fields.One2many("res.partner",'team_id',string="Members (contact)")
+    Responsables = fields.Many2many('res.partner', 'team_contact_rel', 'partner_id', 'team_id' ,domain=_get_domain)
+
