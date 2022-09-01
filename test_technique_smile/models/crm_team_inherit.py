99bae2f507330cb16426a4ebac963f8a0b065315
khallout asmaa <asmaekhalout2015@gmail.com>
2022-09-01 20:37:28 +0100

    test

--- test_technique_smile/models/crm_team_inherit.py
+++ test_technique_smile/models/crm_team_inherit.py
@@ -4,3 +4,4 @@ class CrmTeamInherit(models.Model):
     _inherit = "crm.team"
 
     members_contact = fields.One2many("res.partner",'team_id',string="Members (contact)")
+    Responsables = fields.Many2many('res.partner', 'team_contact_rel', 'partner_id', 'team_id')
