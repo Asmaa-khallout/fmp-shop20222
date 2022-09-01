4de91049155014e034a09363720af4c813925cd4
khallout asmaa <asmaekhalout2015@gmail.com>
2022-09-01 23:50:07 +0100

    test

--- test_technique_smile/models/crm_team_inherit.py
+++ test_technique_smile/models/crm_team_inherit.py
@@ -6,6 +6,6 @@ class CrmTeamInherit(models.Model):
     members_contact = fields.One2many("res.partner",'team_id',string="Members (contact)")
     Responsables = fields.Many2many('res.partner', 'team_contact_rel', 'partner_id', 'team_id')
 
-    @api.models
+    @api.model
     def Responsables_onchange(self):
         return {'domain': {'Responsables': [('id', 'in', self.members_contact)]}}
