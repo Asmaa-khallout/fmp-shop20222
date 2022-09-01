cfee63a73c5c83f1334e6605483c567f2b4409aa
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2022-09-01 16:55:32 +0100

    Add addon

--- theme_prime/static/src/js/core/mixins.js
+++ theme_prime/static/src/js/core/mixins.js
@@ -17,25 +17,6 @@ let DroggolUtils = {
     _getShopConfig: async function () {
         return await this._rpc({ model: 'website', method: 'get_theme_prime_shop_config' });
     },
-
-        _addToCartInPage(params) {
-        params.force_create = true;
-        return this._rpc({
-            route: "/shop/cart/update_json",
-            params: params,
-        }).then(async data => {
-        console("asmaa laa")
-            if (data.cart_quantity && (data.cart_quantity !== parseInt($(".my_cart_quantity").text()))) {
-                await animateClone($('header .o_wsale_my_cart').first(), this.$itemImgContainer, 25, 40);
-                updateCartNavBar(data);
-            }
-            if(data.warning){
-            console.log("oui warning ");
-            console.log(data.warning);
-
-            }
-        });
-    },
 };
 
 let MarkupRecords = {
