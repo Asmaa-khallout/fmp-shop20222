c76d412845f394dfc155ca06e538a6bb2f24c545
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2022-09-01 16:50:05 +0100

    Add addon

--- test_technique_smile/models/res_partner_inherit.py
+++ test_technique_smile/models/res_partner_inherit.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class ResPartnerInherit(models.Model):
     _inherit = "res.partner"
 
-    parent_category_id =  fields.Many2many(related="parent_id.catgory_id",string="category parent")
+    parent_category_id =  fields.Many2many(related="parent_id.category_id",string="category parent")
