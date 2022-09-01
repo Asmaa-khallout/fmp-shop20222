7818a077640041ffee8f91a766afd6e072befdbf
khallout asmaa <asmaekhalout2015@gmail.com>
2022-09-01 22:33:45 +0100

    test

--- test_technique_smile/models/crm_lead_inherit.py
+++ test_technique_smile/models/crm_lead_inherit.py
@@ -1,4 +1,5 @@
 from odoo import models, fields,_ , api
+from datetime import datetime
 
 class CrmLeadInherit(models.Model):
     _inherit = "crm.lead"
@@ -9,3 +10,11 @@ class CrmLeadInherit(models.Model):
 
 
     user_id = fields.Many2one('res.users', domain=_get_user_domain)
+    state_flag = fields.Selection(selection=[('Plus','Plus'),('Moins','Moins')],default="Moins",strin='Statut de flagger')
+
+
+    def flagger_lead(self):
+        lead_ids = self.search([('type','opportunity'),('probability','<',0.4)])
+        lead_ids.filterd(lambda r : (r.create_date- datetime.today()).days >15)
+        lead_ids.write({'state_flag':'Plus'})
+        return
