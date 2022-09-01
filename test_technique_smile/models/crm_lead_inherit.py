ace85c7803584343ba832e9b6b714ef96676769b
khallout asmaa <asmaekhalout2015@gmail.com>
2022-09-01 21:58:38 +0100

    test

--- test_technique_smile/models/crm_lead_inherit.py
+++ test_technique_smile/models/crm_lead_inherit.py
@@ -4,6 +4,6 @@ class CrmLeadInherit(models.Model):
     _inherit = "crm.lead"
 
 
-    @api.onchange('team_id')
+    @api.onchange('user_id')
     def Responsables_onchange(self):
         return {'domain': {'user_id': [('id', 'in', self.team_id.member_ids.ids)]}}
