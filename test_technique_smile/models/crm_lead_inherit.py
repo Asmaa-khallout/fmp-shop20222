163b421452e644b8dd7da9072003f05828bbba8a
khallout asmaa <asmaekhalout2015@gmail.com>
2022-09-01 22:16:27 +0100

    test

--- test_technique_smile/models/crm_lead_inherit.py
+++ test_technique_smile/models/crm_lead_inherit.py
@@ -2,9 +2,10 @@ from odoo import models, fields,_ , api
 
 class CrmLeadInherit(models.Model):
     _inherit = "crm.lead"
-    user_id =  fields.Many2one('res.users' ,domain=lambda self: self._get_user_domain())
 
-
-    @api.onchange('user_id')
+    @api.model
     def _get_user_domain(self):
         return [('id', 'in', self.team_id.member_ids.ids)]
+
+
+    user_id = fields.Many2one('res.users', domain=_get_user_domain)
