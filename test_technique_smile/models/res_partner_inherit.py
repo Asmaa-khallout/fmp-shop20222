bd24b1800536d161c9b379f91de1086b5a08db63
khallout asmaa <asmaekhalout2015@gmail.com>
2022-09-01 23:36:07 +0100

    test

--- test_technique_smile/models/res_partner_inherit.py
+++ test_technique_smile/models/res_partner_inherit.py
@@ -4,4 +4,3 @@ class ResPartnerInherit(models.Model):
     _inherit = "res.partner"
 
     parent_category_id =  fields.Many2many(related="parent_id.category_id",string="category parent")
-    team_test_id=fields.Many2one("crm.team")
