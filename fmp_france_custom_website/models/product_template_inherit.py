e54f597e54ee58a53f53944fc2fa263f8f07af49
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2022-09-09 16:13:24 +0100

    Add addon doofinder

--- fmp_france_custom_website/models/product_template_inherit.py
+++ fmp_france_custom_website/models/product_template_inherit.py
@@ -106,7 +106,7 @@ class ProductTemplateInherit(models.Model):
                     domain2.append([(field, '=', escape_psql(search))])
                     #domains.append(OR(domain2))
 
-        total = AND(domains)
+        total = AND(domains)+OR(domain2)
         total2 = OR(domain2)
         _logger.info("total %s " %(total))
         _logger.info("domain2 %s" %(domain2))
