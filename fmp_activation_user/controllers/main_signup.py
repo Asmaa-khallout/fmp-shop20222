6120611ac4377489a369697de47c321e2bdbf978
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2022-09-12 02:04:01 +0100

    Update Sale order

--- fmp_activation_user/controllers/main_signup.py
+++ fmp_activation_user/controllers/main_signup.py
@@ -31,44 +31,54 @@ class AuthSignupHome(Home):
             attachment_id = request.env['ir.attachment'].sudo().create(attachment_value)
 
     def _prepare_signup_values(self, qcontext):
-        values = { key: qcontext.get(key) for key in ('login', 'name', 'password','phone','mobile','numero_siret','name_shop','vat','street','street2','zip','city','country_id') }
-        qcontext['zip_2']=values['zip']
-        country = 'country_id' in values and values['country_id'] != '' and request.env['res.country'].browse(int(values['country_id']))
-        qcontext['country'] = country and country.exists() or ''
-        if(values['country_id']):
-            values['country_id'] = int(values['country_id'])
-        values['activation_state']="inactive"
-        company_type="person"
-        if(values['numero_siret']):
-            company_type = "company"
-        values['company_type'] = company_type
-        if not values:
-            raise UserError(_("The form was not properly filled in."))
-        if values.get('password') != qcontext.get('confirm_password'):
-            raise UserError(_("Passwords do not match; please retype them."))
-        supported_lang_codes = [code for code, _ in request.env['res.lang'].get_installed()]
-        lang = request.context.get('lang', '')
-        if lang in supported_lang_codes:
-            values['lang'] = lang
-        return values
+        if(request.httprequest.base_url==request.httprequest.url_root+"/web/signup"):
+            values = {key: qcontext.get(key) for key in (
+            'login', 'name', 'password', 'phone', 'mobile', 'numero_siret', 'name_shop', 'vat', 'street', 'street2',
+            'zip', 'city', 'country_id')}
+            qcontext['zip_2'] = values['zip']
+            country = 'country_id' in values and values['country_id'] != '' and request.env['res.country'].browse(
+                int(values['country_id']))
+            qcontext['country'] = country and country.exists() or ''
+            if (values['country_id']):
+                values['country_id'] = int(values['country_id'])
+            values['activation_state'] = "inactive"
+            company_type = "person"
+            if (values['numero_siret']):
+                company_type = "company"
+            values['company_type'] = company_type
+            if not values:
+                raise UserError(_("The form was not properly filled in."))
+            if values.get('password') != qcontext.get('confirm_password'):
+                raise UserError(_("Passwords do not match; please retype them."))
+            supported_lang_codes = [code for code, _ in request.env['res.lang'].get_installed()]
+            lang = request.context.get('lang', '')
+            if lang in supported_lang_codes:
+                values['lang'] = lang
+            return values
+        else:
+            return super(AuthSignupHome)._prepare_signup_values(qcontext)
+
 
     def get_auth_signup_qcontext(self):
-        _logger.info("ssss %s and %s" %(request.httprequest.base_url,request.httprequest.url_root))
-        """ Shared helper returning the rendering context for signup and reset password """
-        qcontext = {k: v for (k, v) in request.params.items() if k in SIGN_UP_REQUEST_PARAMS}
-        qcontext.update(self.get_auth_signup_config())
-        if not qcontext.get('token') and request.session.get('auth_signup_token'):
-            qcontext['token'] = request.session.get('auth_signup_token')
-        if qcontext.get('token'):
-            try:
-                # retrieve the user info (name, login or email) corresponding to a signup token
-                token_infos = request.env['res.partner'].sudo().signup_retrieve_info(qcontext.get('token'))
-                for k, v in token_infos.items():
-                    qcontext.setdefault(k, v)
-            except:
-                qcontext['error'] = _("Invalid signup token")
-                qcontext['invalid_token'] = True
-        return qcontext
+        if(request.httprequest.base_url==request.httprequest.url_root+"/web/signup"):
+            """ Shared helper returning the rendering context for signup and reset password """
+            qcontext = {k: v for (k, v) in request.params.items() if k in SIGN_UP_REQUEST_PARAMS}
+            qcontext.update(self.get_auth_signup_config())
+            if not qcontext.get('token') and request.session.get('auth_signup_token'):
+                qcontext['token'] = request.session.get('auth_signup_token')
+            if qcontext.get('token'):
+                try:
+                    # retrieve the user info (name, login or email) corresponding to a signup token
+                    token_infos = request.env['res.partner'].sudo().signup_retrieve_info(qcontext.get('token'))
+                    for k, v in token_infos.items():
+                        qcontext.setdefault(k, v)
+                except:
+                    qcontext['error'] = _("Invalid signup token")
+                    qcontext['invalid_token'] = True
+            return qcontext
+        else:
+            return super(AuthSignupHome).get_auth_signup_qcontext()
+
 
 
     @http.route('/web/signup', type='http', auth='public', website=True, sitemap=False)
