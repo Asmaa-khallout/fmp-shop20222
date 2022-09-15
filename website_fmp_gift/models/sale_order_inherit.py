29dfa71625665168a12c6a3eebf6563bc37ed4be
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2022-09-15 09:37:58 +0100

    Update Sale order coupon calcul

--- website_fmp_gift/models/sale_order_inherit.py
+++ website_fmp_gift/models/sale_order_inherit.py
@@ -148,6 +148,7 @@ class SaleOrderInherit(models.Model):
 
 
     def _get_reward_values_discount_percentage_per_line(self, program, line):
+        _logger.info("les info line : %s price reduce %s percentage %s" %(line,line.price_reduce,program.discount_percentage))
 
         discount_amount = line.product_uom_qty * line.price_reduce * (program.discount_percentage / 100)
         _logger.info("line price reduce %s  ,, %s " % (line.price_reduce, program.discount_percentage))
